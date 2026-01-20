e6bd8ca62f1fa5de2fdee6749c32b5da08281fbf
Isen Naiken <isen.naiken@nexomedis.com>
2026-01-20 11:54:52 +0100

    update du train.sh

--- train.sh
+++ train.sh
@@ -9,4 +9,18 @@ source /beegfs/data/work/imvia/in156281/clamv2/venv/bin/activate
 module load python
 export PYTHONPATH=/work/imvia/in156281/clamv2/venv/lib/python3.9/site-packages:$PYTHONPATH
 export MPLCONFIGDIR=/work/imvia/in156281/.cache/matplotlib
-python /beegfs/data/work/imvia/in156281/clamv2/train.py --clinical_csv clinical_data.csv --features_dir features --output_dir results
+python /beegfs/data/work/imvia/in156281/clamv2/train.py\
+    --clinical_csv clinical_data.csv \
+    --features_dir features \
+    --output_dir results \
+    --model_type clam_sb \
+    --model_size small \
+    --dropout 0.5 \
+    --lr 0.0002 \
+    --bag_weight 0.7 \
+    --weight_decay 1e-4 \
+    --scheduler cosine \
+    --es_mode loss \
+    --patience 15 \
+    --max_grad_norm 1.0 \
+    --create_new_splits
