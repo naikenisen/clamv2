043413b2e2d095e53e28741a08dac7b7d0e43840
Isen Naiken <isen.naiken@nexomedis.com>
2026-01-20 11:05:42 +0100

    ajusteement du scripte de soumission

--- train.sh
+++ train.sh
@@ -9,4 +9,4 @@ source /beegfs/data/work/imvia/in156281/clamv2/venv/bin/activate
 module load python
 export PYTHONPATH=/work/imvia/in156281/clamv2/venv/lib/python3.9/site-packages:$PYTHONPATH
 export MPLCONFIGDIR=/work/imvia/in156281/.cache/matplotlib
-python /beegfs/data/work/imvia/in156281/clamv2/train.py
+python /beegfs/data/work/imvia/in156281/clamv2/python train.py --clinical_csv clinical_data.csv --features_dir features --output_dir results
